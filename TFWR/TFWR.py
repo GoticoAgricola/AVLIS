3d8b4e071704b760399b2769d68cbb49f46c7eec
GoticoAgricola <129319366+GoticoAgricola@users.noreply.github.com>
2026-02-10 21:02:49 -0300

    Create TFWR.py

---
+++ TFWR/TFWR.py
@@ -0,0 +1,21 @@
+while True:
+	harvest()
+	do_a_flip()
+	harvest()
+	pet_the_piggy()
+	change_hat(Hats.Purple_Hat)
+		while True:
+	if can_harvest():
+		harvest()
+
+    ---------------------------
+while True:
+	harvest()
+	do_a_flip()
+	harvest()
+	pet_the_piggy()
+	change_hat(Hats.Purple_Hat)
+	---------------------------
+	while True:
+	if can_harvest():
+		harvest()
